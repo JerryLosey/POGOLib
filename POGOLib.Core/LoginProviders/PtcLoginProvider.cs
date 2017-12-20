5cf6b80c948a9754eacf6139de69d2f02d785eba
Xelwon <xelwonpgo@gmail.com>
2017-12-21 00:12:43 +0100

    bug in proxy settings fixed

--- POGOLib.Core/LoginProviders/PtcLoginProvider.cs
+++ POGOLib.Core/LoginProviders/PtcLoginProvider.cs
@@ -52,7 +52,7 @@ namespace POGOLib.Official.LoginProviders
             {
                 httpClientHandler.AllowAutoRedirect = false;
                 if (!string.IsNullOrEmpty(_proxyAddress)) {
-                    httpClientHandler.Proxy = new WebProxy(_proxyAddress, true);
+                    httpClientHandler.Proxy = new POGOLib.Official.Extensions.WebProxy(_proxyAddress, true);
                 } 
                 using (var httpClient = new HttpClient(httpClientHandler))
                 {
