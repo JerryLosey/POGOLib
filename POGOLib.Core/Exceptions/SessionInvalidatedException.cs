80314abd77df6613c90aade8610c65f2745a38eb
Furtif <msntchat@hotmail.fr>
2018-01-19 00:31:08 +0100

    Change session invalidate

---
+++ POGOLib.Core/Exceptions/SessionInvalidatedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace POGOLib.Official.Exceptions
+{
+    public class SessionInvalidatedException : Exception
+    {
+        public SessionInvalidatedException()
+        {
+        }
+
+        public SessionInvalidatedException(string message) : base(message)
+        {
+        }
+
+        public SessionInvalidatedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
